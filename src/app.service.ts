afecc8d5a49a3176eda28fd5d38133b22b21e0e7
Regmi C. Mahesh <0xmahadev@gmail.com>
2023-08-19 19:45:14 +0545

    a simple change

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello TestSuperfast CI!';
+    return 'Simple NestJS API';
   }
 }
